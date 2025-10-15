5e1c825b795af77575e790cd2b1ca7d6ec5ea78b
Ermi <ermhut@yea.al>
2025-10-15 12:50:08 +0200

    Added Footer component

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,7 @@
+<script>
+	let year = new Date().getFullYear();
+</script>
+
+<footer class="mt-8 bg-gray-100 text-gray-700 text-sm py-3 px-6 flex justify-center items-center shadow-inner">
+	<p>© {year} Kanban Board. All rights reserved.</p>
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script>
 	import Board from "$lib/components/Board.svelte";
 	import Header from "$lib/components/Header.svelte";
+	import Footer from "$lib/components/Footer.svelte";
 
 </script>
 
 <Header></Header>
 <Board></Board>
+<Footer></Footer>
