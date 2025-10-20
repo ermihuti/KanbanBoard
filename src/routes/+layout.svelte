3335e9201dc26bf07c7cc919514461e80104fcef
Ermi <ErmHut@yea.al>
2025-10-20 19:26:41 +0200

    Added service worker registration to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,18 @@
 <script>
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { onMount } from 'svelte';
 
 	let { children } = $props();
+
+	onMount(() => {
+		if ("serviceWorker" in navigator) {
+			navigator.serviceWorker
+				.register("/service-worker.js")
+				.then(() => console.log("Service Worker registered"))
+				.catch((err) => console.log("SW registration failed:", err));
+		}
+	});
 </script>
 
 <svelte:head>
