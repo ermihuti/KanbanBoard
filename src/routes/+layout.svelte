5704040841af0f34cef7fb7b330519af8c1b7b3c
Ermi <ErmHut@yea.al>
2025-10-21 19:09:38 +0200

    Changed favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script>
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/favicon.png';
 	import { onMount } from 'svelte';
 
 	let { children } = $props();
